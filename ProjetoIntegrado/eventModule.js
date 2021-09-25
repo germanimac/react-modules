528ce5c022362fe8e9bc9dbb477d8d9534756f79
germanimac <49569152+germanimac@users.noreply.github.com>
2021-09-25 11:03:08 -0300

    FINAL

--- ProjetoIntegrado/eventModule.js
+++ ProjetoIntegrado/eventModule.js
@@ -5,10 +5,9 @@ import React from 'react';
 import { NativeModules, Button } from 'react-native';
 const { CalendarModule } = NativeModules;
 const NewModuleButton = () => {
-   
   const onPress = () => {
-    CalendarModule.createCalendarEvent('testName', 'testLocation');
-  }
+    CalendarModule.createCalendarEvent('Dinner Party', 'My House');
+  };
   return (
     <Button title="Click to invoke your native module!" color="#841584" onPress={onPress}/>
   );
